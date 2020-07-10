4c2b21c25750f9ef5e9ec4ec4fe7a4f5e645db0f
MichaelMWang <mmwang2.sy@hotmail.com>
2020-07-10 10:43:00 -0400

    adding job advancement command

---
+++ src/client/command/commands/gm0/JobAdvCommand.java
@@ -0,0 +1,52 @@
+package client.command.commands.gm0;
+
+import client.command.Command;
+import client.MapleClient;
+import client.MapleCharacter;
+import client.MapleJob;
+import java.util.ArrayList;
+
+public class JobAdvCommand extends Command {
+	{
+		setDescription("");
+	}
+
+	@Override
+	public void execute(MapleClient c, String[] params) {
+		MapleCharacter player = c.getPlayer();
+		if (player.getReborns() > 0){
+			MapleJob job = player.getJob();
+			ArrayList<MapleJob> newJob = job.getNextJob();
+			int level = player.getLevel();
+			if (newJob.length == 1){
+				if (level >= newJob[0].getAdvLevel()){
+					player.changeJob(newJob[0]);
+				}
+				else{
+					player.message("You must reach level " + newJob[0].getAdvLevel() + " before advancing.");
+				}
+			}
+			else if (newJob.length == 0){
+				ArrayList<String> newJobNames = MapleJob.getNamesFromIds(newJob)
+				if (params.length > 0){
+					if (newJobNames.contains(param[0])){
+						//player.changeJob(MapleJob.valueOf(param[0].toUpperCase()));
+						player.changeJob(newJob[newJobNames.indexOf(param[0].toLowerCase())]);
+					}
+					else{
+						player.message("Syntax: @rebirth [" + String.join("|",newJobNames) + "]");
+					}
+				}
+				else{
+					player.message("Syntax: @rebirth [" + String.join("|",newJobNames) + "]");
+				}
+			}
+			else{
+				player.message("There are no available job advancements at this time.");
+			}
+		}
+		else{
+			player.message("You must have at least 1 rebirth to use this command.");
+		}
+	}
+}
